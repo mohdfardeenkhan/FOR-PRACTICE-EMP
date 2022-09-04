37cca1103c0c269686203729669f7999208c4dc2
Mohd Fardeen khan <bittufardeen@gmail.com>
2022-09-04 09:52:21 -0700

    registration and login

---
+++ src/test/java/com/bridgelabz/Registration/and/Login/RegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Registration.and.Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
